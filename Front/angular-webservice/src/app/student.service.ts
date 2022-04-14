50553b77cb40fd5429f66d511b2fa4a09599f262
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 <louis.bossard@gmail.com>
2022-04-14 10:38:23 +0200

    echanges pas fonctionnel en fait

--- Front/angular-webservice/src/app/student.service.ts
+++ Front/angular-webservice/src/app/student.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Student } from './student';
@@ -14,15 +14,23 @@ export class StudentService {
   ) { }
 
   getStudents(): Observable<Student[]> {
-    let queryParams = new HttpParams();
-    queryParams = queryParams.append("Access-Control-Allow-Origin","*"); //VERY IMPORTANT
-    return this.http.get<Student[]>(this.url,{params: queryParams});
+    let queryHeader = new HttpHeaders();
+    queryHeader = queryHeader.append("Access-Control-Allow-Origin","*");
+    queryHeader = queryHeader.append('Access-Control-Allow-Headers','Content-Type');
+    queryHeader = queryHeader.append('Access-Control-Allow-Methods','GET,POST,OPTIONS,DELETE,PUT');
+    return this.http.get<Student[]>(this.url,{headers: queryHeader});
   }
 
   deleteUser(id: number): Observable<Object>{
+    let queryHeader = new HttpHeaders();
+    queryHeader = queryHeader.append("Access-Control-Allow-Origin","*");
+
+
+    // 'Access-Control-Allow-Headers': 'Content-Type',
+    //  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+
     let queryParams = new HttpParams();
     queryParams = queryParams.append("id",1); //VERY IMPORTANT
-    queryParams = queryParams.append("Access-Control-Allow-Origin","*"); //VERY IMPORTANT
-    return this.http.delete(this.url,{params: queryParams});
+    return this.http.delete(this.url,{headers: queryHeader ,params: queryParams});
   }
 }
