225dcf8710e7e7482034041a04a75f34eb92964a
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 <louis.bossard@gmail.com>
2022-04-14 10:28:36 +0200

    Échange fonctionnel

--- Front/angular-webservice/src/app/student.service.ts
+++ Front/angular-webservice/src/app/student.service.ts
@@ -7,19 +7,22 @@ import { Student } from './student';
   providedIn: 'root'
 })
 export class StudentService {
-  private url: string = "192.168.151.196:8080/rest/student"
+  private url: string = "http://192.168.151.196:8080/rest/student"
 
   constructor(
     private http: HttpClient
   ) { }
 
   getStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>(this.url)
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append("Access-Control-Allow-Origin","*"); //VERY IMPORTANT
+    return this.http.get<Student[]>(this.url,{params: queryParams});
   }
 
   deleteUser(id: number): Observable<Object>{
     let queryParams = new HttpParams();
     queryParams = queryParams.append("id",1); //VERY IMPORTANT
+    queryParams = queryParams.append("Access-Control-Allow-Origin","*"); //VERY IMPORTANT
     return this.http.delete(this.url,{params: queryParams});
   }
 }
